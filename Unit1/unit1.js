41d81fcedcd11212c855f442dbb304dfc48e90af
tytomyr <art.tytomyr@gmail.com>
2021-09-21 07:53:34 +0300

    editing

---
+++ Unit1/unit1.js
@@ -0,0 +1,16 @@
+function checkIfAllDifferentNumbers (num) {
+    const str = String(num);
+    const arr = [];
+    for (let x=0; x<str.length; x++){
+      arr[x]=str[x];
+        }
+    const arr1 = []
+   for (let z=0; z<arr.length; z++){
+      arr1[z]=Number(arr[z]);
+    }
+    const test = arr1.filter( (x,i,z)=>  z.indexOf(x) == i);
+    if (arr1.length == test.length){
+      return true;
+    } 
+    return false
+  }
---
+++ Unit2/unit2.js
@@ -0,0 +1,5 @@
+function numsProperty (...nums){
+    const arr = nums.map(x =>x);
+    const filtered = arr.filter((num,ind)=>num % 2 === 1 && ind % 2 === 0 );
+    return filtered.length;
+  }
